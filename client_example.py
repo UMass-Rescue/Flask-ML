2d0dccf387ce06e7c777913e618fa1bb0cbce131
prasannals <prasanna.lakkur@gmail.com>
2024-09-16 01:30:05 -0400

    Added more client examples

--- client_example.py
+++ client_example.py
@@ -1,8 +1,12 @@
 from flask_ml.flask_ml_client import MLClient
 from flask_ml.flask_ml_server.constants import DataTypes
 
-url = "http://127.0.0.1:5000/dummymodel"  # The URL of the server
-client = MLClient(url)  # Create an instance of the MLClient object
+DUMMY_MODEL_URL = "http://127.0.0.1:5000/dummymodel"  # The URL of the server
+SENTIMENT_MODEL_URL = "http://127.0.0.1:5000/randomsentimentanalysis"
+IMAGE_STYLE_TRANSFER_MODEL_URL = "http://127.0.0.1:5000/imagestyletransfer"
+
+
+client = MLClient(DUMMY_MODEL_URL)  # Create an instance of the MLClient object
 
 inputs = [
     {"text": "Text to be classified"},
@@ -11,4 +15,19 @@ inputs = [
 data_type = DataTypes.TEXT  # The type of the input data
 
 response = client.request(inputs, data_type)  # Send a request to the server
+print("dummy model response:")
 print(response)  # Print the response
+
+
+client.set_url(SENTIMENT_MODEL_URL)
+response = client.request(inputs, data_type)
+print("sentiment model response:")
+print(response)
+
+
+client.set_url(IMAGE_STYLE_TRANSFER_MODEL_URL)
+data_type = DataTypes.IMAGE
+inputs = [{"file_path": "/path/to/image1.jpg"}, {"file_path": "/path/to/image2.jpg"}]
+response = client.request(inputs, data_type)
+print("image style transfer model response:")
+print(response)
