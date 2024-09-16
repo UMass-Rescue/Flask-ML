dfe8132ff4762569014725ea2a347f9b2f7cbdff
prasannals <prasanna.lakkur@gmail.com>
2024-09-16 01:29:55 -0400

    Added more server examples

--- server_example.py
+++ server_example.py
@@ -1,6 +1,6 @@
 from flask_ml.flask_ml_server import MLServer
 from flask_ml.flask_ml_server.constants import DataTypes
-from flask_ml.flask_ml_server.models import ResponseModel, TextResult
+from flask_ml.flask_ml_server.models import ResponseModel, TextResult, ImageResult
 
 
 # Create a dummy ML model
@@ -9,8 +9,26 @@ class DummyModel:
         return [str(e) for e in range(len(data))]  # Return 0 to len(data) - 1
 
 
+class SentimentModel:
+    def predict(self, data: list[str]) -> list[str]:
+        return [
+            {"text": t.text, "sentiment": "positive" if i % 2 == 0 else "negative"}
+            for i, t in enumerate(data)
+        ]
+
+
+class ImageStyleTransferModel:
+    def predict(self, data: list[str]) -> list[str]:
+        return [
+            {"file_path": f.file_path, "result": f"stylized_image_{i}.jpg"}
+            for i, f in enumerate(data)
+        ]
+
+
 # create an instance of the model
 model = DummyModel()
+sentiment_model = SentimentModel()
+image_style_transfer_model = ImageStyleTransferModel()
 
 # Create a server
 server = MLServer(__name__)
@@ -21,6 +39,22 @@ server = MLServer(__name__)
 def process_text(inputs: list, parameters: dict) -> dict:
     results = model.predict(inputs)
     results = [TextResult(text=e.text, result=r) for e, r in zip(inputs, results)]
+    response = ResponseModel(results=results)
+    return response.get_response()
+
+
+@server.route("/randomsentimentanalysis", DataTypes.TEXT)
+def sentiment_analysis(inputs: list, parameters: dict) -> dict:
+    results = sentiment_model.predict(inputs)
+    results = [TextResult(text=res['text'], result=res['sentiment']) for res in results]
+    response = ResponseModel(results=results)
+    return response.get_response()
+
+
+@server.route("/imagestyletransfer", DataTypes.IMAGE)
+def image_style_transfer(inputs: list, parameters: dict) -> dict:
+    results = image_style_transfer_model.predict(inputs)
+    results = [ImageResult(file_path=res['file_path'], result=res['result']) for res in results]
     response = ResponseModel(results=results)
     return response.get_response()
 
